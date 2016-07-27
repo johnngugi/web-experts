18322aa3d115f981ba535ac26eb2ffccc7a22bf0
johnngugi <johnngugi777@gmail.com>
2016-07-27 08:30:58 +0300

    Add rules for image slider

--- js/script.js
+++ js/script.js
@@ -17,4 +17,22 @@ $(document).ready(function() {
     }
     cycleItems();
   }, 3000);
+
+  $('.next').click(function() {
+  clearInterval(autoSlide);
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+});
+
+$('.prev').click(function() {
+  clearInterval(autoSlide);
+  currentIndex -= 1;
+  if (currentIndex < 0) {
+    currentIndex = itemAmt - 1;
+  }
+  cycleItems();
+});
 });
