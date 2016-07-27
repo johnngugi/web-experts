e71fbc64bca97a5d4b4c944086f6e142835d3682
johnngugi <johnngugi777@gmail.com>
2016-07-27 08:17:19 +0300

    Add more rules

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
+  // alert("Test!");
   var currentIndex = 0;
   var items = $(".container div");
-  var itemAmt = items.length();
+  var itemAmt = items.length;
 
-  var cycleItems = function() {
+  function cycleItems () {
     var item = $(".container div").eq(currentIndex);
+    items.hide();
+    item.css("display", "inline-block");
   }
+
+  var autoSlide = setInterval(function () {
+    currentIndex += 1;
+    if(currentIndex > itemAmt -1) {
+      currentIndex = 0;
+    }
+    cycleItems();
+  }, 3000);
 });
